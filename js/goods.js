91f86a376c96b3e65cf999ce0d6f99ee5e734a85
Nikita Klepikov <nikita.klepikoff@gmail.com>
2018-10-03 00:02:15 +0500

    5.1.3

--- js/goods.js
+++ js/goods.js
@@ -1,18 +1,18 @@
 'use strict';
 //3.1.1
 var NAMES = ['Чесночные сливки', 'Огуречный педант', 'Молочная хрюша', 'Грибной шейк', 'Баклажановое безумие', 'Паприколу итальяно', 'Нинзя-удар васаби',
-'Хитрый баклажан', 'Горчичный вызов','Кедровая липучка', 'Корманный портвейн', 'Чилийский задира', 'Беконовый взрыв', 'Арахис vs виноград',
-'Сельдерейная душа', 'Початок в бутылке', 'Чернющий мистер чеснок', 'Раша федераша', 'Кислая мина', 'Кукурузное утро', 'Икорный фуршет',
-'Новогоднее настроение', 'С пивком потянет', 'Мисс креветка', 'Бесконечный взрыв', 'Невинные винные', 'Бельгийское пенное', 'Острый язычок'];
+    'Хитрый баклажан', 'Горчичный вызов','Кедровая липучка', 'Корманный портвейн', 'Чилийский задира', 'Беконовый взрыв', 'Арахис vs виноград',
+    'Сельдерейная душа', 'Початок в бутылке', 'Чернющий мистер чеснок', 'Раша федераша', 'Кислая мина', 'Кукурузное утро', 'Икорный фуршет',
+    'Новогоднее настроение', 'С пивком потянет', 'Мисс креветка', 'Бесконечный взрыв', 'Невинные винные', 'Бельгийское пенное', 'Острый язычок'];
 
 var IMAGES = ['gum-cedar', 'gum-chile', 'gum-eggplant', 'gum-mustard', 'gum-portwine', 'gum-wasabi', 'ice-cucumber', 'ice-eggplant',
-'ice-garlic', 'ice-italian', 'ice-mushroom', 'ice-pig', 'marmalade-beer', 'marmalade-caviar', 'marmalade-corn', 'marmalade-new-year',
-'marmalade-sour', 'marshmallow-bacon', 'marshmallow-beer', 'marshmallow-shrimp', 'marshmallow-spicy', 'marshmallow-wine', 'soda-bacon',
-'soda-celery', 'soda-cob', 'soda-garlic', 'soda-peanut-grapes', 'soda-russian'];
+    'ice-garlic', 'ice-italian', 'ice-mushroom', 'ice-pig', 'marmalade-beer', 'marmalade-caviar', 'marmalade-corn', 'marmalade-new-year',
+    'marmalade-sour', 'marshmallow-bacon', 'marshmallow-beer', 'marshmallow-shrimp', 'marshmallow-spicy', 'marshmallow-wine', 'soda-bacon',
+    'soda-celery', 'soda-cob', 'soda-garlic', 'soda-peanut-grapes', 'soda-russian'];
 
 var CONTENTS = ['молоко', 'сливки', 'вода', 'пищевой краситель', 'патока', 'ароматизатор бекона', 'ароматизатор свинца',
-'ароматизатор дуба идентичный натуральному', 'ароматизатор картофеля', 'лимонная кислота', 'загуститель', 'эмульгатор',
-'консервант: сорбат калия', 'посолочная смесь: соль, нитрит натрия', 'ксилит', 'карбамид', 'вилларибо', 'виллабаджо'];
+    'ароматизатор дуба идентичный натуральному', 'ароматизатор картофеля', 'лимонная кислота', 'загуститель', 'эмульгатор',
+    'консервант: сорбат калия', 'посолочная смесь: соль, нитрит натрия', 'ксилит', 'карбамид', 'вилларибо', 'виллабаджо'];
 
 var BOOLEANS = [true, false];
 
@@ -21,21 +21,20 @@ var RATINGS = ['stars__rating--one', 'stars__rating--two', 'stars__rating--three
 // случайное целое
 function getRandomNumber(min, max) {
     return Math.round(Math.random() * (max - min) + min);
-};
+}
 
 function getRandomContent(arr){
-  var randomContent = arr[getRandomNumber(0, arr.length - 1)];
+    var randomContent = arr[getRandomNumber(0, arr.length - 1)];
 
-  for (var j = 0; j < getRandomNumber(0, arr.length - 1); j++) {
-    randomContent += ', ' + arr[getRandomNumber(0, arr.length - 1)]
-  }
-  return randomContent;
+    for (var j = 0; j < getRandomNumber(0, arr.length - 1); j++) {
+        randomContent += ', ' + arr[getRandomNumber(0, arr.length - 1)];
+    }
+    return randomContent;
 }
 
 // подъем на несколько родителей
 function gt(node, indx) {
-    var parent;
-    for (var i = 0; i <= indx; i++) {
+    for (i = 0; i <= indx; i++) {
         node = node.parentNode;
     }
     return(node);
@@ -58,10 +57,10 @@ var createArr = function(quantity, arr){
                 energy: getRandomNumber(70, 500),
                 contents:  getRandomContent(CONTENTS)
             }
-        }
+        };
     }
     return arr;
-}
+};
 
 var goods = []; //
 
@@ -118,13 +117,12 @@ var renderGoods = function(goods){
     goodElement.querySelector('.card__composition-list').textContent = goods.nutritionFacts.contents;
 
     return(goodElement);
-}
+};
 
 for (var i = 0; i < goodsQuantity; i++){
     fragment.appendChild(renderGoods(goods[i]));
 }
 catalogCards.appendChild(fragment);
-
 
 //3.1.3 корзина (решение удалено, см. соответствующие коммиты)
 
@@ -132,9 +130,9 @@ var basketCards = document.querySelector('.goods__cards');
 
 var basketTemplate = document.querySelector('#card-order').content.querySelector('.card-order');
 
-//4.1.1 избранное
+//4.1.1 избранное;
 var buttonFavorite = document.querySelectorAll('.card__btn-favorite');
-for(var i = 0; i < buttonFavorite.length; i++){
+for(i = 0; i < buttonFavorite.length; i++){
     buttonFavorite[i].addEventListener('click', function() {
         this.classList.toggle('card__btn-favorite--selected');
     });
@@ -142,12 +140,12 @@ for(var i = 0; i < buttonFavorite.length; i++){
 
 //4.1.2 добавление в корзину
 var buttonAdd = document.querySelectorAll('.card__btn');
-var basketContainer = document.querySelector('.goods__cards');
+// var basketContainer = document.querySelector('.goods__cards');
 var basketGoods = [];
 
 var newFragment = document.createDocumentFragment();
 
-for(var i = 0; i < buttonAdd.length; i++){
+for(i = 0; i < buttonAdd.length; i++){
 
     buttonAdd[i].addEventListener('click', function() { // ставим обработчики
 
@@ -165,7 +163,7 @@ for(var i = 0; i < buttonAdd.length; i++){
             picture: thisCard.querySelector('.card__img').src,
             price: thisCard.querySelector('.card__price').textContent,
             orderedAmount: 1 //количество товара
-        }
+        };
         function testArr(arr, smth){ //проверка на похожесть товара на витрине и в корзине
             for(var j = 0; j < arr.length; j++){
                 if(smth.name === arr[j].name) {
@@ -174,24 +172,24 @@ for(var i = 0; i < buttonAdd.length; i++){
                     return false;
                 }
             }
-            return true
+            return true;
         }
 
 
         if(testArr(basketGoods, basketGood)){
             basketGoods.push(basketGood);
 
-        var basketElement = basketTemplate.cloneNode(true);
-        basketElement.querySelector('.card-order__title').textContent = basketGoods[basketGoods.length - 1].name;
-        basketElement.querySelector('.card-order__price').textContent = basketGoods[basketGoods.length - 1].price;
-        basketElement.querySelector('.card-order__count').value = basketGoods[basketGoods.length - 1].orderedAmount;
+            var basketElement = basketTemplate.cloneNode(true);
+            basketElement.querySelector('.card-order__title').textContent = basketGoods[basketGoods.length - 1].name;
+            basketElement.querySelector('.card-order__price').textContent = basketGoods[basketGoods.length - 1].price;
+            basketElement.querySelector('.card-order__count').value = basketGoods[basketGoods.length - 1].orderedAmount;
 
-        newFragment.appendChild(basketElement);
+            newFragment.appendChild(basketElement);
 
-        basketCards.appendChild(newFragment);
+            basketCards.appendChild(newFragment);
         }
-    })
-};
+    });
+}
 
 /* СДЕЛАТЬ!
 Ещё один момент, который нужно учесть, что в корзину должно быть невозможно добавить больше товаров,
@@ -207,13 +205,13 @@ var deliverCourierTab = document.querySelector('.deliver__courier');
 
 deliverStoreBtn.addEventListener('click', function() {
     deliverStoreTab.classList.remove('visually-hidden');
-    deliverCourierTab.classList.add('visually-hidden')
-})
+    deliverCourierTab.classList.add('visually-hidden');
+});
 
 deliverCourierBtn.addEventListener('click', function() {
     deliverStoreTab.classList.add('visually-hidden');
-    deliverCourierTab.classList.remove('visually-hidden')
-})
+    deliverCourierTab.classList.remove('visually-hidden');
+});
 
 // 4.1.4 обработчик на фильтр, первая фаза
 var rangeBtnLeft = document.querySelector('.range__btn--left');
@@ -222,14 +220,14 @@ var rangeBtnRight = document.querySelector('.range__btn--right');
 var rangePriceMin = document.querySelector('.range__price--min');
 var rangePriceMax = document.querySelector('.range__price--max');
 
-rangeBtnLeft.addEventListener('mouseup', function(evt) {
-    console.log(evt.clientX);
-    console.log(evt.clientY);
+rangeBtnLeft.addEventListener('mouseup', function() {
+    // console.log(evt.clientX);
+    // console.log(evt.clientY);
 });
 
-rangeBtnRight.addEventListener('mouseup', function(evt) {
-    console.log(evt.clientX);
-    console.log(evt.clientY);
+rangeBtnRight.addEventListener('mouseup', function() {
+    // console.log(evt.clientX);
+    // console.log(evt.clientY);
 });
 
 // нужен переключатель, координаты из которого нужно занести в rangePriceMin/max textContent
@@ -249,11 +247,11 @@ rangeBtnLeft.addEventListener('mousedown', function(evt){ // левый
         moveEvt.preventDefault();
 
         var shift = { // смещение
-          x: startCoords.x - moveEvt.clientX,
+            x: startCoords.x - moveEvt.clientX,
         };
 
         startCoords = {
-          x: moveEvt.clientX,
+            x: moveEvt.clientX,
         };
 
         if(((rangeBtnLeft.offsetLeft - shift.x) >= 0) && ((rangeBtnLeft.offsetLeft - shift.x) < rangeBtnRight.offsetLeft - rangeSize)){
@@ -261,7 +259,7 @@ rangeBtnLeft.addEventListener('mousedown', function(evt){ // левый
             rangePriceMin.textContent = rangeBtnLeft.offsetLeft + 24;
 
             rangeFillLine.style.left = (rangeBtnLeft.offsetLeft - shift.x + rangeSize) + 'px';
-            console.log( rangeFillLine.style.left);
+            // console.log( rangeFillLine.style.left);
         }
     };
     var onMouseUp = function (upEvt) {
@@ -270,8 +268,8 @@ rangeBtnLeft.addEventListener('mousedown', function(evt){ // левый
         document.removeEventListener('mousemove', onMouseMove);
         document.removeEventListener('mouseup', onMouseUp);
     };
-  document.addEventListener('mousemove', onMouseMove);
-  document.addEventListener('mouseup', onMouseUp);
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
 });
 
 
@@ -285,11 +283,11 @@ rangeBtnRight.addEventListener('mousedown', function(evt){ // правый
         moveEvt.preventDefault();
 
         var shift = { // смещение
-          x: startCoords.x - moveEvt.clientX,
+            x: startCoords.x - moveEvt.clientX,
         };
 
         startCoords = {
-          x: moveEvt.clientX,
+            x: moveEvt.clientX,
         };
 
         if(((rangeBtnRight.offsetLeft - shift.x) <= 240) && ((rangeBtnRight.offsetLeft - shift.x) > rangeBtnLeft.offsetLeft + rangeSize)){
@@ -305,23 +303,23 @@ rangeBtnRight.addEventListener('mousedown', function(evt){ // правый
         document.removeEventListener('mousemove', onMouseMove);
         document.removeEventListener('mouseup', onMouseUp);
     };
-  document.addEventListener('mousemove', onMouseMove);
-  document.addEventListener('mouseup', onMouseUp);
-})
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+});
 
 
 // 4.2 валидация форм (банковской карты и тд). 1234567890123456
-console.log('4377731425300255  99/99');
+// console.log('4377731425300255  99/99');
 
 var modalClose = document.querySelectorAll('.modal__close');
 var modal = document.querySelectorAll('.modal');
 
-for (var i = 0; i < modal.length; i++){
+for (i = 0; i < modal.length; i++){
     modalClose[i].addEventListener('click', function(){
         modal[1].classList.add('modal--hidden');
         modal[0].classList.add('modal--hidden');
     });
-};
+}
 
 
 function multiplyEvenNumbers(arr){
@@ -338,10 +336,8 @@ function multiplyEvenNumbers(arr){
             arr[j] = arr[j] - 9;
         }
     }
-        return(arr);
+    return(arr);
 }
-
-var result;
 
 var orderBtn = document.querySelector('.buy__submit-btn');
 
@@ -358,20 +354,20 @@ orderBtn.addEventListener('click', function(){
     if (sumResult % 10 !== 0){
         document.querySelector('#payment__card-number').setCustomValidity('У вас поломанная карта');
         //modal[0].classList.remove('modal--hidden');
-        console.log(false)
+        // console.log(false);
 
     } else {
         //modal[1].classList.remove('modal--hidden');
-        console.log(true);
+        // console.log(true);
 
     }
- })
+});
 
 document.querySelector('form').addEventListener('invalid', function(){
-     modal[0].classList.remove('modal--hidden');
-})
+    modal[0].classList.remove('modal--hidden');
+});
 
 orderBtn.addEventListener('submit', function() {
-   // modal[0].classList.remove('modal--hidden');
+    // modal[0].classList.remove('modal--hidden');
     alert('aaaaa');
 });
